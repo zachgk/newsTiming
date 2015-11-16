79bc372f483430428d7ba128dbe7713e121e6806
Zach Kimberg <zachary@kimberg.com>
2015-11-16 13:11:50 -0600

    No fetch output

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 DATE=$(date '+%s')
 MYDIR="$(dirname "$(readlink -f "$0")")"
 err=0
@@ -6,5 +8,5 @@ $MYDIR/genUrls.py $MYDIR
 
 while read -r url filename tail; do
     mkdir -p "$MYDIR/data/$filename"
-    wget -O "$MYDIR/data/$filename/$DATE" "$url" || err=1
+    wget -O "$MYDIR/data/$filename/$DATE" "$url" &> /dev/null || err=1
 done < $MYDIR/urls.txt
